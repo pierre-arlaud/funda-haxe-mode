f634b121d15a9c7c61ac1cfc062b46dcdb17ec62
pierre-arlaud <arlaudpierre@yahoo.fr>
2015-05-03 16:22:32 +0200

    add: GPL v3 license

--- funda-haxe-mode.el
+++ funda-haxe-mode.el
@@ -1,4 +1,25 @@
-;; HaXe Mode by Pierre Arlaud
+;; Funda HaXe Mode
+;; Version: 0.1.0
+;; Author: Pierre Arlaud
+;; URL: https://github.com/pierre-arlaud/funda-haxe-mode
+
+;; ------------------------------------------------------------------------
+;; Copyright (C) 2015 Pierre Arlaud
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope htat it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; ------------------------------------------------------------------------
+
 ;; Supports Syntax highlighting and indenting
 ;; Based on fundamental mode to avoid future breaks
 ;;
